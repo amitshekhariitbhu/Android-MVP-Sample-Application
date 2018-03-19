a2f01e4c1278c43301dcaa11c11866729377ab8b
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2018-03-19 19:39:30 +0530

    Update to 2018

--- app/src/main/java/com/amitshekhar/example/data/remote/ApiEndPoint.java
+++ app/src/main/java/com/amitshekhar/example/data/remote/ApiEndPoint.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *    Copyright (C) 2018 MINDORKS NEXTGEN PRIVATE LIMITED
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
